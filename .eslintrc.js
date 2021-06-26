a853e70ad9ff1f39c5ae58980fad5bc05f11c010
HebaElGalad <heba957@gmail.com>
2021-06-26 23:56:36 +0200

    chore: initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,37 @@
 module.exports = {
   root: true,
+  parser: 'vue-eslint-parser',
   env: {
     node: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: [
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/prettier',
+  ],
+  plugins: ['prettier'],
   parserOptions: {
-    parser: "babel-eslint",
+    parser: 'babel-eslint',
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': ['error', { singleQuote: true, semi: true, printWidth: 120, arrowParens: 'avoid' }],
+    'vue/max-attributes-per-line': [
+      2,
+      {
+        singleline: 20,
+        multiline: {
+          max: 1,
+          allowFirstLine: false,
+        },
+      },
+    ],
+    'vue/singleline-html-element-content-newline': 'off',
+    camelcase: 'off',
+    'vue/comment-directive': 'off',
   },
 };
